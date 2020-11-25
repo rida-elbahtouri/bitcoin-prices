ec81af2e58178540790a4630abdd871972af551d
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-11-25 17:45:21 +0100

    deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get "/bitcoin", to: "bitcoins#bitcoin"
-  get "/chart/:timeper", to: "bitcoins#chart"
-  get "/getvalue", to: "bitcoins#bitcoinvalue"
+  root 'bitcoins#bitcoin'
+  get '/chart/:timeper', to: 'bitcoins#chart'
+  get '/getvalue', to: 'bitcoins#bitcoinvalue'
 end
